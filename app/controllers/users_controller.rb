49432ce33c6893bcebb0b5453e8e2393768111c9
Tai Nguyen <ngn.tai@gmail.com>
2022-05-24 17:33:32 +0200

    user controller créés

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,46 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    user = User.new(user_params)
+    if user.save
+      redirect_to user_path(user)
+    else
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    @user.update(user_params)
+    if @user.save!
+      redirect_to user_path(@user)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @user.destroy
+    redirect_to '#' #route à décider, vu qu'on a pas d'index pour users
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :encrypted_password, :admin)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+end
