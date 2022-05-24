4f06408da3bb4456a9c5506b6309d10a6101c96b
Benjamin Liet <liet.ben@gmail.com>
2022-05-24 14:28:49 +0200

    delete one trip migration + password validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,6 @@ class User < ApplicationRecord
   has_many :bookings
   has_many :my_trips, class_name: 'Trip'
   has_many :trips, through: :bookings
-  validates :first_name, :last_name, :email, :password, presence: true
+  validates :first_name, :last_name, :email, presence: true
   validates :email, uniqueness: true
 end
