66e4e7f2e7bd29f1a385ecaafc301097d00f4fd1
Benjamin Liet <liet.ben@gmail.com>
2022-05-24 12:30:15 +0200

    Created new and create actions to trips controller

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,2 +1,20 @@
 class TripsController < ApplicationController
+  def new
+    @trip = Trip.new
+  end
+
+  def create
+    @trip = Trip.new(trip_params)
+    if @trip.save
+      redirect_to trip_path(@trip)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def trip_params
+    params.require(:trip).permit(:destination, :price_per_day, :min_duration)
+  end
 end
