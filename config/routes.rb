bb3227d9e28ef7eb4a558fdd9564b3f7a7594316
florian lequere <florian.lequere@gmail.com>
2022-05-24 16:19:15 +0200

    1st draft trip views

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  
+
   resources :trips do
     resources :bookings, only: [:new, :create]
   end
-  
+
   resources :bookings, only: [:show, :edit, :update]
 
   resources :users, only: [:show, :new, :create, :edit, :update, :destroy]
