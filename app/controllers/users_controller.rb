c6e439c6585e663886e99eb0cbf6b4dedf530481
Tai Nguyen <ngn.tai@gmail.com>
2022-05-25 12:40:08 +0200

    first_name et last_name ajoutés au formulaire de sign_up, et NEW/CREATE supprimés du controller USER

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,8 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
 
-  def new
-    @user = User.new
-  end
-
-  def create
-    user = User.new(user_params)
-    if user.save!
-      redirect_to user_path(user)
-    else
-      render :new
-    end
+  def index
+    @users = User.all
   end
 
   def show
@@ -31,13 +22,13 @@ class UsersController < ApplicationController
 
   def destroy
     @user.destroy
-    redirect_to :trips_path #route à décider, vu qu'on a pas d'index pour users
+    redirect_to :users_path #route à décider, vu qu'on a pas d'index pour users
   end
 
   private
 
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :email, :password)
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :photo)
   end
 
   def set_user
