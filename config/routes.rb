8040491ec51940ea293d0a98b4cf3be1d9fcd2d8
florian lequere <florian.lequere@gmail.com>
2022-05-25 14:41:42 +0200

    trips update

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,5 @@ Rails.application.routes.draw do
   resources :bookings, only: [:show, :edit, :update]
 
   resources :users, only: [:index, :show, :new, :create, :edit, :update, :destroy]
-
+  get "trips/:id/booked", to: "trips#booked", as: :booked
 end
