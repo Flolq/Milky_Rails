f7906277ef101c87c36a4fa25db4c10c11953738
Tai Nguyen <102608156+ngntai@users.noreply.github.com>
2022-05-25 10:36:32 +0200

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -37,7 +37,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :email, :encrypted_password, :admin)
+    params.require(:user).permit(:first_name, :last_name, :email, :password)
   end
 
   def set_user
