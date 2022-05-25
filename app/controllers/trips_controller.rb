14154545539f3f4eb5898f8290e493ec9d86c70e
florian lequere <florian.lequere@gmail.com>
2022-05-25 10:10:39 +0200

    trip views : form index

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -2,20 +2,26 @@ class TripsController < ApplicationController
   before_action :set_trip, only: [:show, :edit, :update, :destroy]
 
   def index
-    if params[:end_date].nil? && params[:start_date].nil?
+    if params[:end_date].nil? && params[:start_date].nil? && params[:destination].nil?
       @trips = Trip.all
+    elsif params[:end_date] == "" && params[:start_date] == ""
+      destination = params[:destination].downcase.capitalize
+      @trips = Trip.where("destination = ?", destination)
     else
       start_detail = params[:start_date].split("-")
       start_date = Date.new(start_detail[0].to_i, start_detail[1].to_i, start_detail[2].to_i)
       end_detail = params[:end_date].split("-")
       end_date = Date.new(end_detail[0].to_i, end_detail[1].to_i, end_detail[2].to_i)
-      @duration = (end_date - start_date).to_i
-      @destination = params[:destination]
+      duration = (end_date - start_date).to_i
+      destination = params[:destination].downcase.capitalize
       @trips = Trip.where("min_duration < ? and destination = ?", duration, destination)
     end
+    session[:my_params] = params
   end
 
   def show
+    session[:my_params]
+    raise
   end
 
   def new
