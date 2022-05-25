4d0015b4481f781ebd80440ac01e799ef31b15b9
florian lequere <florian.lequere@gmail.com>
2022-05-25 12:11:23 +0200

    trip-views form+show updated

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -4,16 +4,23 @@ class TripsController < ApplicationController
   def index
     if params[:end_date].nil? && params[:start_date].nil? && params[:destination].nil?
       @trips = Trip.all
-    elsif params[:end_date] == "" && params[:start_date] == ""
-      destination = params[:destination].downcase.capitalize
+    elsif (params[:end_date] == "" || params[:start_date] == "") && params[:destination].nil? == false
+      destination = params[:destination].titleize
       @trips = Trip.where("destination = ?", destination)
+    elsif params[:end_date].nil? == false && params[:start_date].nil? == false && params[:destination] == ""
+      start_detail = params[:start_date].split("-")
+      start_date = Date.new(start_detail[0].to_i, start_detail[1].to_i, start_detail[2].to_i)
+      end_detail = params[:end_date].split("-")
+      end_date = Date.new(end_detail[0].to_i, end_detail[1].to_i, end_detail[2].to_i)
+      duration = (end_date - start_date).to_i
+      @trips = Trip.where("min_duration < ?", duration)
     else
       start_detail = params[:start_date].split("-")
       start_date = Date.new(start_detail[0].to_i, start_detail[1].to_i, start_detail[2].to_i)
       end_detail = params[:end_date].split("-")
       end_date = Date.new(end_detail[0].to_i, end_detail[1].to_i, end_detail[2].to_i)
       duration = (end_date - start_date).to_i
-      destination = params[:destination].downcase.capitalize
+      destination = params[:destination].titleize
       @trips = Trip.where("min_duration < ? and destination = ?", duration, destination)
     end
     session[:my_params] = params
@@ -21,7 +28,22 @@ class TripsController < ApplicationController
 
   def show
     session[:my_params]
-    raise
+    if session[:my_params]["start_date"].nil? == false
+      start_detail = session[:my_params]["start_date"].split("-")
+      @start_date = Date.new(start_detail[0].to_i, start_detail[1].to_i, start_detail[2].to_i)
+    end
+    if session[:my_params]["end_date"].nil? == false
+      end_detail = session[:my_params]["end_date"].split("-")
+      @end_date = Date.new(end_detail[0].to_i, end_detail[1].to_i, end_detail[2].to_i)
+    end
+    if session[:my_params]["start_date"].nil? == false && session[:my_params]["end_date"].nil? == false
+      @duration = (@end_date - @start_date).to_i
+    else
+      @duration = 0
+    end
+    if session[:my_params]["number_of_travellers"].nil? == false
+      @number = session[:my_params]["number_of_travellers"]
+    end
   end
 
   def new
